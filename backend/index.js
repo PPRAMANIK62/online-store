c54939faaf9cfd6a2060ce82800bc4ad0421e4f2
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-03-25 00:23:43 +0530

    FEAT - image upload done

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
-import path from "path";
+import path, { dirname } from "path";
 
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import { fileURLToPath } from "url";
 
 dotenv.config({ path: "backend/.env" });
 const port = process.env.PORT || 5000;
@@ -19,9 +21,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/api/users', userRoutes);
-app.use('/api/category', categoryRoutes);
-app.use('/api/products', productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/upload", uploadRoutes);
+
+let __dirname = dirname(fileURLToPath(import.meta.url));
+__dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
