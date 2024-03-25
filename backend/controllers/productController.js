d0dc8b6f59f1b626e4896434034ed75bd67a0d20
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-03-25 15:21:26 +0530

    FEAT - product craete with frontend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -25,7 +25,7 @@ const addProduct = asyncHandler(async (req, res) => {
 
     const product = new Product({ ...req.fields });
     await product.save();
-    res.status(201).json({ product });
+    res.status(201).json(product);
   } catch (error) {
     console.error(error);
     return res.status(404).json(error.message);
@@ -61,7 +61,7 @@ const updateProductDetails = asyncHandler(async (req, res) => {
     );
 
     await product.save();
-    res.status(200).json({ product });
+    res.status(200).json(product);
   } catch (error) {
     console.error(error);
     return res.status(404).json(error.message);
@@ -110,7 +110,7 @@ const fetchProductsById = asyncHandler(async (req, res) => {
   try {
     const product = await Product.findById(req.params.id);
     if (product) {
-      return res.status(200).json({ product });
+      return res.status(200).json(product);
     } else {
       res.status(404);
       throw new Error("Product not found");
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,6 +13,7 @@ import Register from "./pages/Auth/Register.jsx";
 import Profile from "./pages/User/Profile.jsx";
 import store from "./redux/store.js";
 import CategoryList from "./pages/Admin/CategoryList.jsx";
+import ProductList from "./pages/Admin/ProductList.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,6 +28,7 @@ const router = createBrowserRouter(
       <Route path="/admin" element={<AdminRoute />}>
         <Route path="userlist" element={<UserList />} />
         <Route path="categorylist" element={<CategoryList />} />
+        <Route path="productlist" element={<ProductList />} />
       </Route>
     </Route>
   )
